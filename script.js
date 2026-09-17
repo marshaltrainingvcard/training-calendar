daaeb9f3058fd151bca67584b57f18be3723cf08
marshaltrainingvcard <marshaltrainingfeedback@gmail.com>
2026-09-17 13:23:48 +0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const apiURL =
 
 const calendar = document.getElementById("calendar");
 const pagination = document.getElementById("pagination");
-const itemsPerPage = 20; // UPDATED TO 20 ITEMS PER PAGE
+const itemsPerPage = 10; // UPDATED TO 20 ITEMS PER PAGE
 let currentPage = 1;
 let allCourses = [];
 
