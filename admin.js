5b4de95104b8f1f6448e2fd282bd2a2de03a0bde
marshaltrainingvcard <marshaltrainingfeedback@gmail.com>
2026-09-17 10:55:43 +0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -66,3 +66,10 @@ document.getElementById("addCourseForm").onsubmit = async (e) => {
 
 // Load courses on page start
 loadCourses();
+function formatDate(d) {
+    const date = new Date(d);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+}
